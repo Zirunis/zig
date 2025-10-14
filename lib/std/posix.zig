c9c9026a258cd9dfc42cc10d58675049346f33a2
Andrew Kelley <andrew@ziglang.org>
2025-10-13 23:36:44 -0700

    compiler: update for introduction of std.Io
    only thing remaining is using libc dns resolution when linking libc

--- lib/std/posix.zig
+++ lib/std/posix.zig
@@ -5524,6 +5524,8 @@ pub const RealPathError = error{
     /// On Windows, the volume does not contain a recognized file system. File
     /// system drivers might not be loaded, or the volume may be corrupt.
     UnrecognizedVolume,
+
+    Canceled,
 } || UnexpectedError;
 
 /// Return the canonicalized absolute pathname.
@@ -5588,7 +5590,6 @@ pub fn realpathZ(pathname: [*:0]const u8, out_buffer: *[max_path_bytes]u8) RealP
             error.FileLocksNotSupported => unreachable,
             error.WouldBlock => unreachable,
             error.FileBusy => unreachable, // not asking for write permissions
-            error.InvalidUtf8 => unreachable, // WASI-only
             else => |e| return e,
         };
         defer close(fd);
